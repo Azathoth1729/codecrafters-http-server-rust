a4a54b96e1a2bad04f4cfb00cd7bba2449dcb983
Azathoth1729 <aa1203528272@gmail.com>
2024-05-05 13:47:29 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    let ip_arr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 4221);
+    let listener = TcpListener::bind(ip_arr).unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        let mut stream = stream?;
+        println!("accepted new connection");
+
+        let http_response = "HTTP/1.1 200 OK\r\n\r\n";
+        stream.write(http_response.as_bytes())?;
+        let mut buffer = String::new();
+        stream.read_to_string(&mut buffer)?;
+        println!("received:\n{buffer}")
     }
+    Ok(())
 }
