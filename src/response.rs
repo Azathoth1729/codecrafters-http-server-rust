92c8ce13daa91c1147ce4a033b955de9043cceb3
Azathoth1729 <aa1203528272@gmail.com>
2024-05-07 05:22:13 +0800

    codecrafters submit [skip ci]

--- src/response.rs
+++ src/response.rs
@@ -1,27 +1,71 @@
-use std::fmt;
-use std::fmt::{Debug, Formatter};
+use std::fmt::{Debug};
 
-pub trait InComingBody {}
+use bytes::Bytes;
+use format_bytes::format_bytes;
 
-impl InComingBody for () {}
 
-impl InComingBody for str {}
+pub type BodyData = Option<Bytes>;
 
+#[derive(Debug)]
 pub struct Response<T> {
     inner: hyper::Response<T>,
 }
 
-impl<T> fmt::Display for Response<T> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        self.inner.version().fmt(f)?;
-        write!(
-            f,
-            " {} {}\r\n\r\n",
+impl<T> Response<T> {
+    pub fn serialize_without_body(&self) -> Vec<u8> {
+        let header_str = self
+            .inner
+            .headers()
+            .iter()
+            .flat_map(|(name, value)| {
+                format_bytes!(
+                    b"{}: {}\r\n",
+                    name.to_string().as_bytes().to_vec(),
+                    value.as_bytes().to_vec()
+                )
+            })
+            .collect::<Vec<u8>>();
+        let start_line = format!(
+            "{:?} {} {}\r\n",
+            self.inner.version(),
             self.inner.status().as_str(),
             self.inner.status().canonical_reason().unwrap()
-        )
+        );
+        format_bytes!(b"{}{}\r\n", start_line.as_bytes(), header_str)
     }
 }
+
+impl Response<()> {
+    pub fn serialize(&self) -> Vec<u8> {
+        self.serialize_without_body()
+    }
+}
+
+impl Response<BodyData> {
+    pub fn serialize(&self) -> Vec<u8> {
+        let mut serialize_str_without_body = self.serialize_without_body();
+        let body = self.inner.body();
+        let body_str = if let Some(bytes) = body {
+            std::str::from_utf8(bytes).unwrap()
+        } else {
+            ""
+        };
+        serialize_str_without_body.extend(body_str.as_bytes());
+        serialize_str_without_body
+    }
+}
+
+// impl<T> fmt::Display for Response<T> {
+//     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+//         write!(
+//             f,
+//             "{:?} {} {}\r\n\r\n",
+//             self.inner.version(),
+//             self.inner.status().as_str(),
+//             self.inner.status().canonical_reason().unwrap()
+//         )
+//     }
+// }
 
 impl<T> From<hyper::Response<T>> for Response<T> {
     fn from(value: hyper::Response<T>) -> Self {
@@ -31,23 +75,52 @@ impl<T> From<hyper::Response<T>> for Response<T> {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use hyper::header::{CONTENT_LENGTH, CONTENT_TYPE};
     use hyper::http::response;
+    use hyper::StatusCode;
+
+    use super::*;
 
     #[test]
-    fn response_display_test() -> anyhow::Result<()> {
+    fn response_serialize_test() -> anyhow::Result<()> {
         assert_eq!(
-            Response::from(response::Builder::new().status(200).body(())?)
-                .to_string()
-                .as_bytes(),
+            Response::from(response::Builder::new().status(StatusCode::OK).body(())?).serialize(),
             b"HTTP/1.1 200 OK\r\n\r\n"
         );
         assert_eq!(
-            Response::from(response::Builder::new().status(404).body(())?)
-                .to_string()
-                .as_bytes(),
+            Response::from(
+                response::Builder::new()
+                    .status(StatusCode::NOT_FOUND)
+                    .body(())?
+            )
+            .serialize(),
             b"HTTP/1.1 404 Not Found\r\n\r\n"
         );
+        assert_eq!(
+            Response::from(
+                response::Builder::new()
+                    .status(StatusCode::NOT_FOUND)
+                    .body(Some("abc".into()))?
+            )
+            .serialize(),
+            b"HTTP/1.1 404 Not Found\r\n\r\nabc"
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn response_serialize_test2() -> anyhow::Result<()> {
+        let body_bytes = Bytes::from("abc");
+        let response = Response::from(
+            response::Builder::new()
+                .status(StatusCode::OK)
+                .header(CONTENT_TYPE, "text/plain")
+                .header(CONTENT_LENGTH, body_bytes.len())
+                .body(Some(body_bytes))?,
+        );
+        let expected =
+            b"HTTP/1.1 200 OK\r\ncontent-type: text/plain\r\ncontent-length: 3\r\n\r\nabc";
+        assert_eq!(response.serialize().as_bytes(), expected);
         Ok(())
     }
 }
